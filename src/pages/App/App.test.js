c58a0470d4642319b6dafa9cc88adb5415f90e0c
Daniel Carlson <dancarl758@gmail.com>
2020-05-05 19:03:05 +0100

    Updated snapshot test for entire app

--- src/pages/App/App.test.js
+++ src/pages/App/App.test.js
@@ -1,7 +1,12 @@
-import React from 'react';
-import { render } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import App from './App'
+import renderer from 'react-test-renderer'
 
-test('renders learn react link', () => {
-  
-});
+describe(`UI: Render correctly`, () => {
+  it('renders correctly', () => {  
+    const tree = renderer  
+    .create(<App />)  
+    .toJSON();  
+      expect(tree).toMatchSnapshot();  
+    });
+})
