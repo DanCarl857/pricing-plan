bb178e0ce334076b351b225afd9b2fc3d82dbfdb
Daniel Carlson <dancarl758@gmail.com>
2020-05-05 18:56:42 +0100

    Updates

---
+++ precache-manifest.b4e58e75ae5098ecf06c50e671dc8f2c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c82ff5e6d6f9c21a340d702c18f5cb57",
+    "url": "/pricing-plan/index.html"
+  },
+  {
+    "revision": "de1ae1f0c07eb4a3e466",
+    "url": "/pricing-plan/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "b4021d1a50210c4fa183",
+    "url": "/pricing-plan/static/css/main.d7676597.chunk.css"
+  },
+  {
+    "revision": "de1ae1f0c07eb4a3e466",
+    "url": "/pricing-plan/static/js/2.7d6f7f35.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/pricing-plan/static/js/2.7d6f7f35.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b4021d1a50210c4fa183",
+    "url": "/pricing-plan/static/js/main.5556272b.chunk.js"
+  },
+  {
+    "revision": "0188da8e35b1ddd90504",
+    "url": "/pricing-plan/static/js/runtime-main.2a2b9852.js"
+  }
+]);
